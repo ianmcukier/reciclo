5ecf0632230b14cb6b59ef10c5b88e72337140ea
bastosgabriel <gab-bass@poli.ufrj.br>
2018-11-02 23:21:00 -0300

    Initial rewards commit

---
+++ client/src/components/Rewards/Rewards.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Rewards = props => {
+    return (
+        <div>
+            premios
+        </div>
+    );
+};
+
+Rewards.propTypes = {
+    
+};
+
+export default Rewards;
--- client/src/containers/User/User.js
+++ client/src/containers/User/User.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Contributions from '../../components/Contributions/Contributions';
+import Rewards from '../../components/Rewards/Rewards';
 
 
 const dados = {
@@ -7,11 +8,13 @@ const dados = {
   peso: 2,
   lixo: "cocô",
 }
+
 class User extends Component {
   render() {
     return (
       <div>
         <Contributions {...dados}/>
+        <Rewards/>
       </div>
     );
   }
