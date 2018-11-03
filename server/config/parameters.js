2638bc922fd1530b534706a2edcd7f9147c54559
Ian <ianmcukier@gmail.com>
2018-11-03 14:47:51 -0200

    Adicionado mongodb atlas

--- server/config/parameters.js
+++ server/config/parameters.js
@@ -2,6 +2,7 @@
 const _PARAMETERS = {
     MONGO:{
         URL:"mongodb://127.0.0.1/reciclo",
+        URL2:"mongodb+srv://bacanos:1234@reciclo-nsfbd.mongodb.net/test?retryWrites=true",
         PORT : 27017,
         COLLECTIONS: {
             USER:"user",
